4840398a8b99dc713638cd040bda7a9ae201a73c
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-07-01 22:07:29 +0200

    Added the link to the console

--- extension.js
+++ extension.js
@@ -1,36 +1,48 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require("vscode");
+const axios = require("axios");
+const { XMLParser } = require("fast-xml-parser");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
+  const res = await axios.get("https://blog.webdevsimplified.com/rss.xml");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "search-blog" is now active!');
+  const parsedData = new XMLParser();
+  const articles = parsedData
+    .parse(res.data)
+    .rss.channel.item.map((article) => ({
+      label: article.title,
+      detail: article.description,
+      link: article.link,
+    }));
+  console.log(articles);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('search-blog.searchBlog', function () {
-		// The code you place here will be executed every time your command is executed
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "search-blog" is now active!');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Search Blog!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "search-blog.searchBlog",
+    async function () {
+      const article = await vscode.window.showQuickPick(articles, {
+        matchOnDetail: true,
+      });
 
-	context.subscriptions.push(disposable);
+      console.log(article);
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
